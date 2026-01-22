4c03589b23113d809caaea51f2b8ce42057687d2
August <faintlightswitch@gmail.com>
2026-01-22 02:51:44 +0000

    oops

--- BfresToCast/Program.cs
+++ BfresToCast/Program.cs
@@ -17,7 +17,7 @@ public class Program
             while (true)
             {
                 Console.WriteLine("Please input the path to the input file to convert.");
-                path = Console.ReadLine();
+                path = Console.ReadLine().Trim('"');
                 if (path == "exit")
                     return;
 
