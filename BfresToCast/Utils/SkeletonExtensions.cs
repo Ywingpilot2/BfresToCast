bb9649d9fbe8d6726f66ece2f258f83016e271cf
August <faintlightswitch@gmail.com>
2026-01-22 01:08:23 +0000

    big rework, support sarc and yaz0

--- BfresToCast/Utils/SkeletonExtensions.cs
+++ BfresToCast/Utils/SkeletonExtensions.cs
@@ -1,10 +1,10 @@
 ﻿using BfresLibrary;
 
-namespace BFResToCast;
+namespace BfresToCast.Utils;
 
 public static class SkeletonExtensions
 {
-    public static ushort GetBoneIndex(this Skeleton skeleton, float bone) => GetBoneIndex(skeleton, (int)bone);
+    public static ushort GetBoneIndex(this Skeleton skeleton, float bone) => skeleton.GetBoneIndex((int)bone);
     
     public static ushort GetBoneIndex(this Skeleton skeleton, int weightId)
     {
